2f0bbc34e9c31627571687f3fa12fa36374fd625
SidMorad <SidMorad@ubuntu10.04>
2014-06-04 13:58:09 +0000

    refs #8 Help page improvements.  * Simple introudction  * Game objective  * Moving blocks  sub(help)pages added as of this revision.

--- main/src/mars/clocki/interfaces/HelpActivity.java
+++ main/src/mars/clocki/interfaces/HelpActivity.java
@@ -1,7 +1,12 @@
 package mars.clocki.interfaces;
 
 import mars.clocki.R;
+import mars.clocki.interfaces.help.HelpPageFragment;
+import mars.clocki.interfaces.help.IntroducationFragment;
+import mars.clocki.interfaces.help.MovingBlocksFragment;
+import mars.clocki.interfaces.help.ObjectiveFragment;
 import android.os.Bundle;
+import android.view.View;
 
 public class HelpActivity extends AbstractActivity {
 
@@ -10,6 +15,53 @@ public class HelpActivity extends AbstractActivity {
     super.onCreate(savedInstanceState);
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     setContentView(R.layout.activity_help);
+
+    if (savedInstanceState != null) {
+      return;
+    }
+
+    HelpPageFragment fragment = new HelpPageFragment();
+    fragment.setArguments(getIntent().getExtras());
+
+    getSupportFragmentManager().
+      beginTransaction().
+      add(R.id.help_container, fragment, helpHomePage).
+      commit();
   }
+
+  public void goToIntroduction(View view) {
+    getSupportFragmentManager().
+      beginTransaction().
+      replace(R.id.help_container, new IntroducationFragment()).
+      addToBackStack(null).
+      commit();
+  }
+
+  public void goToObjective(View view) {
+    getSupportFragmentManager().
+      beginTransaction().
+      replace(R.id.help_container, new ObjectiveFragment()).
+      addToBackStack(null).
+      commit();
+  }
+
+  public void goToMovingBlocks(View view) {
+    getSupportFragmentManager().
+      beginTransaction().
+      replace(R.id.help_container, new MovingBlocksFragment()).
+      addToBackStack(null).
+      commit();
+  }
+
+  public void backTo(View view) {
+    getSupportFragmentManager().
+    beginTransaction().
+      replace(R.id.help_container,
+              getSupportFragmentManager().
+                findFragmentByTag(helpHomePage)).
+      commit();
+  }
+
+  private final String helpHomePage = "helpHomePage";
 
 }
---
+++ main/src/mars/clocki/interfaces/help/IntroducationFragment.java
@@ -0,0 +1,18 @@
+package mars.clocki.interfaces.help;
+
+import mars.clocki.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class IntroducationFragment extends Fragment {
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.help_introduction, container, false);
+  }
+
+}
